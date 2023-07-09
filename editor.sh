e7bc9eb866757d9e06f6de0f70d50643d4f1ff58
Joshua Carter <joshua.carter@postgrad.manchester.ac.uk>
2023-07-09 15:58:18 +0100

    Improved wording and layout. Now requires variable state descriptors

--- editor.sh
+++ editor.sh
@@ -2,29 +2,32 @@
 #$-cwd
 
 supported=("Morrison" "ISHMAEL" "P3")
-read -p "Would you like me to perform some edits? " confirm
+echo ""
+read -p "Would you like me to perform some edits? [y/n]: " confirm
 if [ $confirm == y ]; then
 
     #Check Variable is present
     var_oi=$1
     if [[ -n "$var_oi" ]]; then
-        echo "Variable to be applied is $var_oi"
+        echo ""
+        echo "--------> Variable to be extracted is  ===> $var_oi"
         
-        read -p "Which microphysics scheme? 
-        Morrison
-        ISHMAEL
-        P3
-        : " schemetype  
+        read -p "Which microphysics scheme? [ Morrison / ISHMAEL / P3 ]: " schemetype  
         
         #check scheme is supported
         if [[ " ${supported[*]} " =~ " ${schemetype} " ]]; then
-            echo "Ok! I will perform the edits on $schemetype"
+            echo ""
+            echo "--------> Ok! I will perform the edits on $schemetype"
     
 
             if [ $schemetype == "Morrison" ]; then 
+            
+                read -p "What are the SI units of this variable? " units
+                read -p "What would you like the NETCDF keyword to be for this variable? " NETCDFNAME
+                read -p "Provide a short description of this variable: " descriptor
                 echo ""
-                echo "Opening Morrison_editor python file..."
-                python3 mp_scheme_scripts/Morrison_editor.py $var_oi
+                echo "--------> Opening Morrison_editor python file..."
+                python3 mp_scheme_scripts/Morrison_editor.py $var_oi $units $NETCDFNAME $descriptor
             else
                 echo "Sorry, this programme does not support edits to this scheme at this time. The available options are:"
                 printf '%s\n' "${supported[@]}"
