9a5f4617c04bd0ef20ed09dabe771460384c31a9
Joshua Carter <joshua.carter@postgrad.manchester.ac.uk>
2023-07-09 12:08:29 +0100

    Exit program at earliest possible stage of error

--- editor.sh
+++ editor.sh
@@ -4,29 +4,34 @@
 supported=("Morrison" "ISHMAEL" "P3")
 read -p "Would you like me to perform some edits? " confirm
 if [ $confirm == y ]; then
-    read -p "Which microphysics scheme? 
-    Morrison
-    ISHMAEL
-    P3
-    : " schemetype
-    
-    if [[ " ${supported[*]} " =~ " ${schemetype} " ]]; then
-    # whatever you want to do when array contains value
-    echo "Ok! I will perform the edits on $schemetype"
-    else
-    echo "Sorry, this programme does not support edits to this scheme at this time. The available options are:"
-    printf '%s\n' "${supported[@]}"
-    fi
-fi
 
-#Morrison edits here:::
-if [ $schemetype == "Morrison" ]; then 
-    echo ""
-    echo "Opening Morrison_editor python file..."
-
+    #Check Variable is present
     var_oi=$1
     if [[ -n "$var_oi" ]]; then
-        python3 mp_scheme_scripts/Morrison_editor.py $var_oi
+        echo "Variable to be applied is $var_oi"
+        
+        read -p "Which microphysics scheme? 
+        Morrison
+        ISHMAEL
+        P3
+        : " schemetype  
+        
+        #check scheme is supported
+        if [[ " ${supported[*]} " =~ " ${schemetype} " ]]; then
+            echo "Ok! I will perform the edits on $schemetype"
+    
+
+            if [ $schemetype == "Morrison" ]; then 
+                echo ""
+                echo "Opening Morrison_editor python file..."
+                python3 mp_scheme_scripts/Morrison_editor.py $var_oi
+            else
+                echo "Sorry, this programme does not support edits to this scheme at this time. The available options are:"
+                printf '%s\n' "${supported[@]}"
+            fi
+     
+        fi 
+        
     else
         echo ""
         echo "--------------------------"
@@ -34,8 +39,15 @@ if [ $schemetype == "Morrison" ]; then
         echo "--------------------------"
         echo ""
         echo " Please ensure that a variable is specified"
-    fi 
+    fi
+    
+    
+else
+    echo "Exiting..."
 fi
 
-echo "Program Close"
+
+echo ""
+echo ""
+echo "!!! PROGRAM END !!!"
 
